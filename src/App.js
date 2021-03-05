c595eb50da5edc2bd7209d79eae52b2c5ff5b908
brunommbruno <brunommbruno@gmail.com>
2021-03-05 23:50:34 +0000

    created navbar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Component } from "react";
 import { HashRouter, Route, Switch } from "react-router-dom";
 
+import Nav from "./Components/Nav";
+
 class App extends Component {
   render() {
     return (
       <HashRouter basename="/">
         <>
+          <Nav />
           <Switch></Switch>
         </>
       </HashRouter>
---
+++ src/Components/Nav.js
@@ -0,0 +1,21 @@
+import { Navbar } from "react-bootstrap";
+import logo from "../img/logo.png";
+
+const Nav = () => {
+  return (
+    <Navbar bg="white" variant="light">
+      <Navbar.Brand href="#home">
+        <img
+          alt=""
+          src={logo}
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{" "}
+        Pages
+      </Navbar.Brand>
+    </Navbar>
+  );
+};
+
+export default Nav;
